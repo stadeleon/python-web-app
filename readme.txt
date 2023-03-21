0036c913c606d94cb426ade35aee5e2f0dffe2dd
leonid.derevianko <leonid.derevianko@promenade.co>
2023-03-21 12:33:03 +0100

    gitignore fix

--- readme.txt
+++ readme.txt
@@ -6,4 +6,10 @@ Then create repo on GitHub and push the project
 to deploy run the app-server
 `streamlit run web.py`
 open the page and in hamburger-menu select "Deploy this app"
+and it will open the https://share.streamlit.io/ page
 
+if you are not authorised  or this site is not authorised in your GitHub signin and authorise
+than select the correct repo, branch and press "Deploy" button
+
+in my case it passed me to this page when Deployed
+https://stadeleon-python-web-tasklist-web-69sh7i.streamlit.app/
--- web.py
+++ web.py
@@ -3,7 +3,7 @@ import functions
 
 todos = functions.get_todos()
 
-
+st.set_page_config(layout="wide")
 def add_todo():
     todo = st.session_state["new_todo"]
     todos.append(todo + "\n")
@@ -12,7 +12,12 @@ def add_todo():
 
 st.title("My Todo App")
 st.subheader("This is subheader for the app")
-st.write("This app is to increase our productivity and python knowledge")
+st.write(
+    "This app is to increase our <b>productivity</b> and python knowledge",
+    unsafe_allow_html=True)
+
+st.text_input(label="Enter a task here", placeholder="Enter a task here",
+              on_change=add_todo, key='new_todo')
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=index)
@@ -22,5 +27,3 @@ for index, todo in enumerate(todos):
         del st.session_state[index]
         st.experimental_rerun()
 
-st.text_input(label="Enter a task here", placeholder="Enter a task here",
-              on_change=add_todo, key='new_todo')
